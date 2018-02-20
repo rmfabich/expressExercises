a92b51d0fcabd68deab32d3caa37fa189052b9ed
kramduckner <buckner.a.mark@gmail.com>
2018-02-20 11:35:11 -0700

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,6 @@
+//copy and paste the boilerplate from a previous file. 
+
+//using the openWeather API, grab some data from it and serve it through some routes and handlers. 
+//Use postman to grab and view the data. 
+
+//This is what a proxy server is - a server that acts as a middleman - in this case it is between openWeather and a client.
