a83a61837424ec4127681835bba284a662b86427
kramduckner <buckner.a.mark@gmail.com>
2018-02-20 12:04:42 -0700

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,1 +1,15 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(express.static('public'));
+app.use(bodyParser.json({type:'application/json'}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.listen(5000, function() {
+   console.log("Listening on 5000");
+});
