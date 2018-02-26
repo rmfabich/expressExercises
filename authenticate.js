13746f1345391545dccda156b5ab0526bc54e6b3
[Rebecca Fabich] <[rmfabich@gmail.com]>
2018-02-26 16:35:49 -0700

    End of Day

--- authenticate.js
+++ authenticate.js
@@ -29,18 +29,37 @@ const db = [{
 //compare whether the password is correct. You also need to pass in a string as the second arg into jwt.sign(), 
 //which will act as your secret
 
+ function verifyToken(req, res, next) {
+   var token = req.body.token;
+    if (token){
+        jwt.verify(token, "Secreet", (err, decode)=>{
+            if (err) {
+                res.send("Wrong token")
+            } else {
+                console.log(decode);
+                res.locals.decode = decode
+             next();
+            }
+        })
+    }   else {
+        res.send("No token")
+    }
+}
+app.post("/getData", verifyToken, (req, res)=> {
+    res.json("sensitive top secret NSA Crack data")
+})
 app.get("/", (req, res) => {
     res.sendfile('index.html');
 });
 
-app.get("/authenticate", (req, res) =>{
+app.post("/authenticate", (req, res) =>{
     var match = db.find((entry)=>{
-        if (entry.userName === req.body.userName){
+        if (entry.userName === req.body.userName && entry.password === req.body.password){
             return true;
         }
      });
       if (match){
-            var token = jwt.sign(user, app.get(''), {
+            var token = jwt.sign(req.body.userName, ('Secreet'), {
             //expiresInMinutes: 1440 // expires in 24 hours, no longer valid, probs deprecated
          });
         res.json({
@@ -50,12 +69,13 @@ app.get("/authenticate", (req, res) =>{
       }
 });
 
+
 //You'll actually NEVER want to store passwords as plain text - you'll always hash them. Import a hashing library, 
 //make a /create route and handler that puts a new user into the db const - however, you'll have to use
 //a hashing library. You'll also need to modify the authenticate handler to compare hashes rather than plaintext.
 //note - this will break every other entry in your database, but it should work for any new entries you create. 
 
 
-app.listen(5000, function() {
-   console.log("Listening on 5000");
+app.listen(8080, function() {
+   console.log("Listening on 8080");
 });
--- proxy.js
+++ proxy.js
@@ -4,3 +4,26 @@
 //Use postman to grab and view the data. 
 
 //This is what a proxy server is - a server that acts as a middleman - in this case it is between openWeather and a client.
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var axios = require ("axios");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function apiPull(req, res, next){
+    axios.get("http://api.openweathermap.org/data/2.5/weather?q=bozeman&APPID=df763ac8b1b29ebbbf6e5d41aa8d44eb").then((response)=> {    
+        res.locals.temp = Math.floor(response.data.main.temp * (9/5) - 459.67);
+        next();
+    }) 
+}
+
+app.get("/getWeather", apiPull, (req, res)=>{
+    console.log(res.locals.temp)
+        res.json(`The temperature is currently ${res.locals.temp}`);
+})
+
+app.listen(8080, function() {
+    console.log("listening on port 8080");
+});
